f61cbdcd47b8a0d90c467997adc4685d46463ebb
Dixon <dixonmunosmurillo1@gmail.com>
2024-10-31 00:07:55 -0600

    subiendo los cambios del back al repositorio

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ require('./config/passport'); // Importa la configuración de Passport
 
 const authRoutes = require('./routes/auth');    // Importa el archivo auth.js
 const oauthRoutes = require('./routes/oauth');  // Importa el archivo oauth.js
+const categoryRoutes = require('./routes/Category'); // Importa las rutas de categoría
+const subcategoryRouter = require('./routes/subcategory'); // Importa el router de subcategorías
+
+const cors = require('cors');
+
+
+
+
 
 // Importa los modelos aquí
 const User = require('./models/User');
@@ -27,15 +35,27 @@ const Client_Raffle = require('./models/Client_Raffle');
 const Raffle = require('./models/Raffle');
 
 
+
 const app = express();
 
+const corsOptions = {
+    origin: 'http://localhost:3000', // Cambia esto al origen de tu aplicación frontend
+    credentials: true, // Permitir el envío de cookies y credenciales
+};
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
 
+
 // Rutas
 app.use('/auth', authRoutes);    // Configura las rutas de autenticación JWT
 app.use('/oauth', oauthRoutes);  // Configura las rutas de autenticación OAuth
+app.use('/categories', categoryRoutes);
+app.use('/subcategories', subcategoryRouter); // Usa el router de subcategorías
+
 
 // Sincronización de la base de datos
 const syncDatabase = async () => {
