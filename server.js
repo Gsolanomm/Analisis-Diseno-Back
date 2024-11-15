40db4d8311da8c9c0d7c2eb6d4c499eb573ca0e3
Gsolanomm <geraldsolano04@gmial.com>
2024-11-15 07:58:12 -0600

    Notification Ready

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ const subcategoryRouter = require('./routes/subcategory'); // Importa el router 
 const salesRoutes = require('./routes/sales'); // Importa las rutas de ventas
 const tablesRoutes = require('./routes/Tables'); // Importa las rutas de mesas
 const dishRoutes = require('./routes/Dish'); // Importa las rutas de platos
-const raffleRoutes = require('./routes/raffle'); // Importa las rutas de rifa
-const ReservationRoutes = require('./routes/Reservation'); // Importa las rutas de reservaciones
-
+const NotificationRoutes = require('./routes/Notifications'); // Importa las rutas de notificaciones
 
 // Importa los modelos aquí
 const User = require('./models/User');
@@ -34,12 +32,12 @@ const Notification = require('./models/Notification');
 const Reservation = require('./models/Reservation');
 const Menu_Category = require('./models/Menu_Category');
 const Dish_SubCategory = require('./models/Dish_SubCategory');
-const Notice = require('./models/Notice');
+const Notice =  require('./models/Notice');
 const Client_Raffle = require('./models/Client_Raffle');
 const Raffle = require('./models/Raffle');
 const sales = require('./models/Sales');
 const Tables = require('./models/Tables');
-const reservation = require('./models/Reservation');
+const Notifications = require('./models/Notification');
 
 const app = express();
 
@@ -50,7 +48,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
     origin: 'http://localhost:3000', // Cambia esto por el dominio de tu frontend
     credentials: true, // Para permitir el envío de cookies
-}));
+  }));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -66,24 +64,16 @@ app.use('/subcategories', subcategoryRouter); // Usa el router de subcategorías
 app.use('/sales', salesRoutes); // Usa el router de ventas
 app.use('/tables', tablesRoutes); // Usa el router de mesas
 app.use('/dish', dishRoutes); // Usa el router de platos
-app.use('/raffle', raffleRoutes); // Usa el router de rifa
-app.use('/reservations', ReservationRoutes); // Usa el router de reservaciones
+app.use('/notifications', NotificationRoutes); // Configura las rutas de notificaciones
 
 
-const notice = require('./routes/notices');
-app.use('/notices', notice);
-
-const recipe = require('./routes/recipe');
-app.use('/recipes', recipe);
-
-const review = require('./routes/review');
-app.use('/reviews', review);
-
+const notice = require('./routes/notices'); 
+app.use('/notices', notice); 
 
 // Sincronización de la base de datos
 const syncDatabase = async () => {
     try {
-        await sequelize.sync();
+        await sequelize.sync(); // 'force: true' elimina las tablas existentes y las vuelve a crear
         console.log('Base de datos sincronizada');
     } catch (error) {
         console.error('Error al sincronizar la base de datos:', error);
